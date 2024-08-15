9c9499fcf00dae289d780ca361bf419c30f1eec2
jyoti <jyotiprakash.cse@gmail.com>
2024-08-15 12:01:36 +0530

    mobile design changes content

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,7 +14,7 @@ const About = () => {
           <p>
             Hi, my name is <b>Jyoti Prakash Nayak</b> and I am from Odisha, India.
             I'm a <b>Front End web developer</b> and I have done my <br/><b>B-Tech in CSE</b>. <br/><br/>
-            I have started my career as a <b>Software Developer</b> at IBM which.
+            I have started my career as a <b>Software Developer</b> at IBM which,
             I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
             I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
             Apart from coding I love to do photography, you can check out some of my shots here &nbsp;
